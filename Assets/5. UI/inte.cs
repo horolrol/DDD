06579e1eb343859332a48cf101fca7c15d9102d0
horolrol <horolrol3089@gmail.com>
2025-08-09 18:26:35 +0900

    DDD

---
+++ Assets/5. UI/inte.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class inte : MonoBehaviour, IInteractable
+{
+    public string GetDescription()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}
